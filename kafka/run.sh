170d08922cc4559f8c13e662e4918b12e5ce6ce7
felipearmat <felipearmat@gmail.com>
2021-10-21 18:54:39 -0300

    Setting and sharing some variables with .env file

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-export ZK_SERVER=zookeeper:2181
-export KFK_SERVER=kafka:9092
-export KFK_PARTITIONS=10
-export KFK_REPLICATION=1
-
 declare -a topicos=(
-    "mensagem.publico"
-    "mensagem.gm")
+    "messages.publ"
+    "messages.adm")
 
 # cria um tópico
 if [ $1 = create ]; then
-    kafka-topics --zookeeper $ZK_SERVER --create --replication-factor $KFK_REPLICATION --partitions $3 --topic $2 --config "retention.ms=-1"
+    kafka-topics --zookeeper $KAFKA_ZOOKEEPER_CONNECT --create --replication-factor $KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR --partitions $3 --topic $2 --config "retention.ms=-1"
 
 # deleta um tópico
 elif [ $1 = delete ]; then
-    kafka-topics --zookeeper $ZK_SERVER --delete --topic $2
+    kafka-topics --zookeeper $KAFKA_ZOOKEEPER_CONNECT --delete --topic $2
 
 # imprime as informações de um tópico
 elif [ $1 = desc ]; then
-    kafka-topics --zookeeper $ZK_SERVER --describe --topic $2
+    kafka-topics --zookeeper $KAFKA_ZOOKEEPER_CONNECT --describe --topic $2
 
 # lista todos os tópicos
 elif [ $1 = list ]; then
-    kafka-topics --zookeeper $ZK_SERVER --list
+    kafka-topics --zookeeper $KAFKA_ZOOKEEPER_CONNECT --list
 
 # delete e recria um tópico
 elif [ $1 = reset ]; then
@@ -34,7 +29,7 @@ elif [ $1 = reset ]; then
 elif [ $1 = create-all ]; then
     for i in "${topicos[@]}"
     do
-        ./run.sh create "$i" $KFK_PARTITIONS
+        ./run.sh create "$i" $KAFKA_PARTITIONS
     done
 
 # deleta todos os tópicos
@@ -55,42 +50,42 @@ elif [ $1 = reset-all ]; then
 
 # imprime todas as mensagens de um tópico
 elif [ $1 = print ]; then
-    kafka-console-consumer --bootstrap-server $KFK_SERVER --topic $2 --isolation-level read_committed --property "print.timestamp=true" --property "print.key=true" --property "print.key=true" --property key.separator=" : " --from-beginning
+    kafka-console-consumer --bootstrap-server $KAFKA_SERVER --topic $2 --isolation-level read_committed --property "print.timestamp=true" --property "print.key=true" --property "print.key=true" --property key.separator=" : " --from-beginning
 
 # retorna um produtor que recebe mensagens e as adiciona no tópico especificado
 elif [ $1 = put ]; then
-    kafka-console-producer --broker-list $KFK_SERVER --topic $2 --property "parse.key=true" --property "key.separator=:"
+    kafka-console-producer --broker-list $KAFKA_SERVER --topic $2 --property "parse.key=true" --property "key.separator=:"
 
 # Retorna o offset atual de um tópico
 elif [ $1 = offset ]; then
-    kafka-run-class kafka.tools.GetOffsetShell --broker-list $KFK_SERVER --topic $2 --time -1
+    kafka-run-class kafka.tools.GetOffsetShell --broker-list $KAFKA_SERVER --topic $2 --time -1
 
 # delete um grupo de consumo
 elif [ $1 = gdelete ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --delete
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --delete
 
 # imprime as informações de um grupo de consumo
 elif [ $1 = gdesc ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --describe
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --describe
 
 # lista todos os grupos de consumo
 elif [ $1 = glist ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --list
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --list
 
 # volta o grupo de consumo para a PRIMEIRA mensagem de TODOS os tópicos
 elif [ $1 = greset ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --execute --reset-offsets --all-topics --to-earliest
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --execute --reset-offsets --all-topics --to-earliest
 
 # passa o grupo de consumo para a ÚLTIMA mensagem de TODOS os tópicos
 elif [ $1 = greset-latest ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --execute --reset-offsets --all-topics --to-latest
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --execute --reset-offsets --all-topics --to-latest
 
 # volta o grupo de consumo para a PRIMEIRA mensagem do tópico
 elif [ $1 = gresettopic ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --execute --reset-offsets --topic $3 --to-earliest
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --execute --reset-offsets --topic $3 --to-earliest
 
 # passa o grupo de consumo para a ÚLTIMA mensagem do tópico
 elif [ $1 = gresettopic-latest ]; then
-    kafka-consumer-groups --bootstrap-server $KFK_SERVER --group $2 --execute --reset-offsets --topic $3 --to-latest
+    kafka-consumer-groups --bootstrap-server $KAFKA_SERVER --group $2 --execute --reset-offsets --topic $3 --to-latest
 
 fi
